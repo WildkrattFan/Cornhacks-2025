a38ef47323e2326a9727169450f1e604a28a0b78
WildkrattFan <88059692+WildkrattFan@users.noreply.github.com>
2025-03-01 13:39:25 -0600

    additional comments

--- my-app/src/routes/+page.server.ts
+++ my-app/src/routes/+page.server.ts
@@ -1,23 +1,26 @@
 import type { PageServerLoad } from './$types';
 import { isRedirect, redirect } from '@sveltejs/kit';
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
-
 
 export const actions = {
-    //Posts the details to the postAPI
+    /**
+     * postDetails to the postAPI and redirects to the launch page if successful
+     * if not successful, redirects to the error page
+     * 
+     * @param request
+     * @throws {redirect}
+     * @returns {Promise<any>}
+     */
     postDetails: async ({ request }: { request: Request }) => {
 
         try {
-            console.log("entering the posting");
             const formData = await request.formData();
 
             let data = {
 
             }
-            console.log("random time is " + formData.get("randomTime"));
+
+            //checks if the time is random or not
             if (formData.get("randomTime") != "null") {
                 let randomTime = new Date(Date.now() + Math.floor(Math.random() * 1000 * 60 * 60 * 24)).toISOString();
 
@@ -35,7 +38,6 @@ export const actions = {
                     image: formData.get("image")
                 }
             }
-            console.log(data);
 
             //posts the data to the postAPI
 
@@ -47,10 +49,14 @@ export const actions = {
             //     body: JSON.stringify(data)
             // });
 
+            //redirects to the launch page
             throw redirect(302,"/launch");
         } catch (err) {
             if(isRedirect(err)){
                 throw redirect(err.status,err.location);
+            }
+            else{
+                throw redirect(500,"/error");
             }
         }
     }
---
+++ my-app/src/routes/error/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+</script>
+
+<div class = "body">
+    <h3>An error occured during launch!</h3>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    :global(body) {
+        font-family: 'Arial', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        overflow-x: hidden;
+        overflow-y: hidden;
+
+    }
+    .body{
+        background-color: black;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        color: white;
+        flex-direction: column;
+    }
+    a{
+        color: white;
+        text-decoration: none;
+        font-size: 20px;
+    }
+    a:hover{
+        color: red;
+    }
+</style>
